f272192014bc21146ddd0b7c18eb89a3ea20f1ee
Gil Caspi <gilicaspi@gmail.com>
2022-09-21 17:58:09 -0500

    Super Basic Skeleton - nothing yet usable

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainContentComponent } from './main-content/main-content.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainContentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
