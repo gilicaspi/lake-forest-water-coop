5f4cf43f75bb4e8074f8adf2c01b724c5aa0fcaf
Gil Caspi <gilicaspi@gmail.com>
2022-12-25 20:33:13 -0600

    Fix title

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,10 +7,10 @@ const Home: React.FC = () => {
   return (
     <Layout>
       <Head>
-        <title>TEST TITLE</title>
-        <meta name='og:title' content='TEST TITLE' />
+        <title>Lake Forest CCR Document</title>
+        <meta name='og:title' content='Lake Forest CCR Document' />
         {/* TODO: Gil Note: Implement meta description for SEO? */}
-        <meta name='description' content='Lake forest water cooop' />
+        <meta name='description' content='Lake forest water co-op consumer confidence report' />
       </Head>
       <MainContent />
     </Layout>
